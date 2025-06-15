157064fe26757d4ab4f062f166541ba3ff6bbf40
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-14 21:24:55 -0500

    Update static and media URLs to include leading slash
    Add secure session and CSRF cookies for production

--- proyecto_django/proyecto_django/settings/local.py
+++ proyecto_django/proyecto_django/settings/local.py
@@ -17,11 +17,15 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR /'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Carpeta donde se copiarán
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'
+
+if not DEBUG:
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
