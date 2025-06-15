57e91bafd1b1ef96747dadae64ed709d4c855c05
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2025-06-14 21:25:01 -0500

    Add production settings for Django project

---
+++ proyecto_django/proyecto_django/settings/prod.py
@@ -0,0 +1,28 @@
+from .base import *
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Carpeta donde se copiarán
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'
